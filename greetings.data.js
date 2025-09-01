64579b56dcb9eab61819e13807ca843af6e433a2
Louis Cerda <louisdcerda@gmail.com>
2025-09-01 13:12:00 -0400

    removed ola

--- greetings.data.js
+++ greetings.data.js
@@ -1,4 +1,4 @@
 // A simple global list—no wrappers, no modules.
 window.HELLO_GREETINGS = [
-  "Hello","안녕하세요","Hola","Bonjour","Hallo","Ciao","Olá", "Hey"
+  "Hello","안녕하세요","Hola","Bonjour","Hallo","Ciao", "Hey"
 ];
